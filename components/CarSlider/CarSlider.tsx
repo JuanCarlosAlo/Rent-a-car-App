d5eb1a4bd74429e9240372e081deba6604144d96
JC <design2@proximediaspain.es>
2024-10-24 17:59:45 +0200

    slider car

--- components/CarSlider/CarSlider.tsx
+++ components/CarSlider/CarSlider.tsx
@@ -1,17 +1,42 @@
+// components/CarSlider.tsx
+'use client';
+
 import { CAR_LIST } from "@/lib/carPh";
 import { CarDetails, CarImage, PriceTag, SliderContainer } from "./styles";
+import { Swiper, SwiperSlide } from 'swiper/react'; 
+import SwiperCore from 'swiper'; 
+import 'swiper/swiper-bundle.css'; 
+import { Navigation, Autoplay } from 'swiper/modules';
+
+SwiperCore.use([Autoplay, Navigation]);
+
+const getCarsToShow = () => {
+  const carsOnSale = CAR_LIST.filter(car => car.isOnsale);
+  return carsOnSale.length > 0 
+    ? carsOnSale 
+    : CAR_LIST.sort(() => 0.5 - Math.random()).slice(0, 5); 
+};
 
 const CarSlider = () => {
-    const carsOnSale = CAR_LIST.filter(car => car.isOnsale);
+  const carsToShow = getCarsToShow();
 
-    const carsToShow = carsOnSale.length > 0 
-      ? carsOnSale 
-      : CAR_LIST.sort(() => 0.5 - Math.random()).slice(0, 5); 
-  
-      return (
-        <SliderContainer>
-          {carsToShow.map(car => (
-            <div key={car._id}>
+  return (
+    <SliderContainer>
+      <Swiper
+        direction={"horizontal"}
+        spaceBetween={30} // Espacio entre las cards
+        slidesPerView={'auto'} // Permite que se muestren todas las cards posibles
+        loop={true} 
+        width={350}
+        autoplay={{
+          delay: 2000, 
+          disableOnInteraction: false,
+        }}
+        navigation={true} 
+      >
+        {carsToShow.map((car) => (
+          <SwiperSlide key={car._id} style={{ width: 'auto', flex: '0 0 auto' }}>
+            <div style={{ width: '300px' }}> 
               <CarImage src={car.cover} alt={`${car.brand} ${car.model}`} />
               <CarDetails>
                 <h3>{`${car.brand} ${car.model}`}</h3>
@@ -19,9 +44,11 @@ const CarSlider = () => {
                 <PriceTag>{`${car.price} ${car.divisa} / mes`}</PriceTag>
               </CarDetails>
             </div>
-          ))}
-        </SliderContainer>
-      );
-    };
-  
-  export default CarSlider;
+          </SwiperSlide>
+        ))}
+      </Swiper>
+    </SliderContainer>
+  );
+};
+
+export default CarSlider;
--- components/CarSlider/styles.ts
+++ components/CarSlider/styles.ts
@@ -3,30 +3,31 @@ import styled from 'styled-components';
 
 export const SliderContainer = styled.div`
   display: flex;
-  overflow-x: auto;
-  scroll-snap-type: x mandatory;
-  padding: 1rem;
-  gap: 1rem; 
-
-  & > div {
-    flex: 0 0 auto;
-    width: 550px;
-    padding:2rem;
-    border-radius: 8px;
-    box-shadow: 0 4px 8px rgba(0, 0, 0, 0.2);
-    overflow: hidden;
-    transition: transform 0.3s;
-    background-color: #fff; 
-
-    &:hover {
-      transform: translateY(-5px); /* Efecto de elevación al pasar el ratón */
-    }
-  }
+  width: 100%; 
+  position: relative; 
+  gap:2rem;
+  margin: 2rem 0;
+ 
 `;
+
+export const SliderCard = styled.div`
+  flex: 0 0 auto;
+  width:550px;
+  padding:2rem;
+  border-radius: 8px;
+  box-shadow: 0 4px 8px rgba(0, 0, 0, 0.2);
+  overflow: hidden;
+  transition: transform 0.3s;
+  background-color: #fff; 
+
+  &:hover {
+    transform: translateY(-5px); 
+  }
+`
 
 export const CarImage = styled.img`
   width: 100%;
-  height: 200px; /* Ajusta la altura según tus necesidades */
+  height: 200px;
   object-fit: contain; /* Mantiene la proporción de la imagen */
 `;
 
